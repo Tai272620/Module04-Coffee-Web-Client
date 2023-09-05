183f32d2d4871333021bc3e391a515e93acdabe1
Tai272620 <tai272620967@gmail.com>
2023-09-05 14:02:15 +0700

    register, login feature

---
+++ src/pages/admins/components/AdminNavbar/AdminNavbar.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function AdminNavbar() {
+    return (
+        <div>
+            <span>Customers</span>
+            <span>Products</span>
+        </div>
+    )
+}
